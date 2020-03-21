d57489f52f96d0b26efe4d8c30da1fbf8e87c2b5
Pierre WSL2 <woodman@DESKTOP-T3GUSOS.localdomain>
2020-03-21 13:40:10 +0800

    UPDATE - Rename Camera component to CapturePhoto

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import BarcodeScan from "./component/barcodeScan/BarcodeScan";
 import ItemDetails from "./component/itemDetails/ItemDetails";
 import ItemSaved from "./component/itemSaved/ItemSaved";
-import Camera from "./component/camera/Camera";
+import CapturePhoto from "./component/capturePhoto/CapturePhoto";
 import { AppRegistry } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 
@@ -17,7 +17,7 @@ function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={BarcodeScan} />
-          <Stack.Screen name="Item-photo" component={Camera} />
+          <Stack.Screen name="Item-photo" component={CapturePhoto} />
           <Stack.Screen name="Item-details" component={ItemDetails} />
           <Stack.Screen name="Item-saved" component={ItemSaved} />
         </Stack.Navigator>
