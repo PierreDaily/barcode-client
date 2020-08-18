6e618df2f36482e2a3780e9efd4866dfb6b46280
Pierre DAILY <contact@pierredaily.com>
2020-08-18 17:29:38 +0800

    UPDATE - test update

--- features/item-saved/components/ItemSaved.test.js
+++ features/item-saved/components/ItemSaved.test.js
@@ -24,14 +24,20 @@ test("renders correctly", () => {
   expect(tree).toMatchSnapshot();
 });
 
-test("navigates to Home screen on button press", () => {
-  const { queryAllByA11yRole } = render(
-    <ItemSaved navigation={navigationMock} />
-  );
+test("navigates to Home screen on 'No' button press", () => {
+  const { getByTestId } = render(<ItemSaved navigation={navigationMock} />);
 
-  const homeButton = queryAllByA11yRole("button");
-  expect(homeButton).toHaveLength(1);
+  const noButton = getByTestId("noBtn");
 
-  fireEvent.press(homeButton[0]);
+  fireEvent.press(noButton);
   expect(navigationMock.navigate).toHaveBeenCalledWith("Home");
+});
+
+test("navigates to Add-price screen on 'Yes' button press", () => {
+  const { getByTestId } = render(<ItemSaved navigation={navigationMock} />);
+
+  const yesButton = getByTestId("yesBtn");
+
+  fireEvent.press(yesButton);
+  expect(navigationMock.navigate).toHaveBeenCalledWith("Add-price");
 });
